0dd9413bd9e599ca2e103400165fcf6e5c2dd098
Jamie Evans <taichi.chikung@gmail.com>
2017-10-12 01:04:00 +0000

    adding....

--- titlecase.js
+++ titlecase.js
@@ -1,13 +1,13 @@
 function titleCase(str) {
-  str = str.split(' ');
+  str = str.split(' '); //split string into array
 
-  for (i = 0; i < str.length; i++) {
-    str[i] = str[i].toLowerCase().split('');
-    str[i][0] = str[i][0].toUpperCase();
+  for (i = 0; i < str.length; i++) { //loop array
+    str[i] = str[i].toLowerCase().split(''); //convert to lowrcase
+    str[i][0] = str[i][0].toUpperCase(); //convert to uppercase
     str[i] = str[i].join('');
   }
 
-  return str.join(' ');
+  return str.join(' '); //join back as array
 }
 
 titleCase("I'm a Little tea pot");
